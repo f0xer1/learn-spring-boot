9eaa92239b0d97652d8459b93b93e0e847dc6a55
Pavlo Ilnitskyi <inavlo092@gmail.com>
2023-10-07 13:54:36 +0300

    first commit

---
+++ src/test/java/com/f0xer/learnspringboot/LearnSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f0xer.learnspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
